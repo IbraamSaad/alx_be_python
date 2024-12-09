4af3cd445058b83f1f610ddf0655f806c6f6a686
IbraamSaad <ebramb37@gmail.com>
2024-12-09 16:55:28 -0500

    python basic operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+# basic Arithmetic project
+# assign 2 VARs for each number 
+# VAR 1
+number1 = 10
+#VAR2
+number2 = 5
+
+#Addition variables
+print (f"Addition of {number1} and {number2} is {number1+number2}")
+
+#Subtraction variables
+print (f"Substraction of {number1} and {number2} is {number1-number2}")
+
+#Multiplication variables
+print (f"Multiplication of {number1} and {number2} is {number1*number2}")
+
