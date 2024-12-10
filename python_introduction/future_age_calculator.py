57c6541ce9211cd139d0df2cc2ef81931a53d986
IbraamSaad <ebramb37@gmail.com>
2024-12-10 15:49:40 -0500

    future age calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,15 @@
+# future age calculator program
+
+# aske user about his age and store it in VAR
+user = int (input ("How old are you?\n"))
+
+# substract the current year with future year (2050)
+future_age = 2050-2023
+
+# addtion the user age and future age
+age = user + future_age
+
+# print the result
+print (f"In 2050,  you will be {age} years old.")
+
+
