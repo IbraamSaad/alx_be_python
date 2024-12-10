f616431cc7eed12508edd573c0b34ee85e4786c0
IbraamSaad <ebramb37@gmail.com>
2024-12-10 14:25:24 -0500

    Calculation the area of rectangle program

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,15 @@
+# rectangle area program
+
+# assign 2 VARS, length and width with 10 and 5 value.
+
+# length VAR
+length = 10
+
+# width VAR
+width = 5
+
+# assign area VAR to multiple length * width
+Area = length * width
+
+# print Area
+print (f"The area of the rectangle is : {Area}")
