179bccbed6907b7035635abf139054fb5beae626
IbraamSaad <ebramb37@gmail.com>
2024-12-10 13:50:27 -0500

    annual interest rate script.

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,17 @@
+# calculates the simple interest earned on an investment over a period of time.
+# assign 3 VARS for principal, rate and time 
+
+# priciaple VAR with $1000
+principal = 1000
+
+# rate VAR with 0.05 annual interest rate
+rate = 0.05
+
+# time VAR for time invested in a period time
+time = 3
+
+# multibly each variable to get a imple interesr in 3 year
+interest = float (principal * rate * time)
+
+# print the interest VAR
+print (interest)
