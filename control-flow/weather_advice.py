c17e4778c4db3d5599b7f49e8c8146ee82b533c3
IbraamSaad <ebramb37@gmail.com>
2024-12-16 18:07:07 +0200

    conditional statment with python script:

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,19 +1,10 @@
-# weather advice program
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-# Print the clothing recommendation based on the weather condition provided by the use.
-
-# predfined weather (sunny/rainy/cold)
-print ("weather condition is: sunny, rainy or cold")
-
-# define a VAR and prompot the user to to tell about weather case today.
-weather = input ("What's the weather like today? (sunny/rainy/cold): ")
-
-# use conditional statement 
 if weather == "sunny":
-           print (" Wear a t-shirt and sunglasses.")
+    print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
-           print ("Dont forget your umbrella and a raincoat.")
+    print("Don't forget your umbrella and a raincoat.")
 elif weather == "cold":
-           print ("Make sure to wear a warm coat and a scarf.")
+    print("Make sure to wear a warm coat and a scarf.")
 else:
-           print ("Sorry, I don't have recommendations for this weather.")
+    print("Sorry, I don't have recommendations for this weather.")
