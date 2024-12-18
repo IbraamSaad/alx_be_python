aedc9e88abd63953bb75072504e46b7730efbb5d
IbraamSaad <ebramb37@gmail.com>
2024-12-18 22:09:48 +0200

    conditional statment with python script:

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,32 +3,27 @@ num2 = int(input ("Enter second num: "))
 
 operation = input("Choose your operation (+, -, *, /): ")
 
-operation_list = ["+", "-", "*", "/"]
 
 result = int()
-trails = 1
-while trails > 0:
 
- if operation not in operation_list:
-  operation = input("Choose your operation (+, -, *, /): ")
- else:
+if num2 == 0 and operation == "/":
+    print ("Cannot divided by zero.")
+else:
+
   match operation:
-   case "/":
-    if num2 == 0:
-     print ("Cannot divided by zero.")
-     break
-    else:
-     result = num1 / num2
-   case "+":
-    result = num1 + num2
-   case "-":
-    result = num1 - num2
-   case "*":
-    result = num1 * num2
-  trails -= 1
+    
+    case "/":
+      result = num1 / num2
+    case "+":
+      result = num1 + num2
+    case "-":
+      result = num1 - num2
+    case "*":
+      result = num1 * num2
+    
   print (f"The result is: {result}.")
-   
-  
+
+    
 
 
         
