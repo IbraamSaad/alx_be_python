f32afd0a4a173ce0b133bdbef6fe03912bef4882
IbraamSaad <ebramb37@gmail.com>
2024-12-18 23:29:16 +0200

    draw patter scipt

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,16 @@
+pattern = int(input("Enter the size of the pattern: "))
+row = 0
+while row < pattern:
+    for _ in range(pattern):
+        print ("*", end="")
+    print()
+    row += 1
+    if row == pattern:
+        break
+        
+    
+    
+  
+
+    
+    
