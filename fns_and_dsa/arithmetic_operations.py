f28bd63bc87e6d782aa4db1037abce20de35070c
IbraamSaad <ebramb37@gmail.com>
2025-01-10 18:17:13 +0200

    arithmetic_operation function script

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,17 +5,17 @@ def perform_operation(num1, num2, operation):
 
 
         match operation:
-            case "/":
+            case "/" | "divide":
                 if num2 == 0:
                     print ("Cannot divided by zero.")
                     break
                 else:
                     result = num1 / num2
-            case "+":
+            case "+" | "add":
                 result = num1 + num2
-            case "-":
+            case "-" | "substract":
                 result = num1 - num2
-            case "*":
+            case "*" | "multiply":
                 result = num1 * num2
         return result
     
