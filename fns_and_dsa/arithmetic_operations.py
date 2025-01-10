ef04bbef26cb44578aa555a03a34ab4a3395ec2c
IbraamSaad <ebramb37@gmail.com>
2025-01-10 18:33:31 +0200

    arithmetic_operation function script

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,33 +1,18 @@
 def perform_operation(num1, num2, operation):
     result = 0
 
-    while True:
+    match operation:
+        
+        case "add":
+            result = num1 + num2
+        case "substract":
+            result = num1 - num2
+        case "multiply":
+            result = num1 * num2
+        case "divide":
+            if num2 == 0:
+                print("Cannot divided by zero")
+            elif num2 != 0:
+              result = num1 / num2
 
-
-        match operation:
-            case "/" | "divide":
-                if num2 == 0:
-                    print ("Cannot divided by zero.")
-                    break
-                else:
-                    result = num1 / num2
-            case "+" | "add":
-                result = num1 + num2
-            case "-" | "substract":
-                result = num1 - num2
-            case "*" | "multiply":
-                result = num1 * num2
-        return result
-    
-
-def main():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
-
-if __name__ == "__main__":
-    main()
+    return result
