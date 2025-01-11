a136464c1d4c9e756fae3f2c0e64823ce2689d15
IbraamSaad <ebramb37@gmail.com>
2025-01-12 00:21:28 +0200

    datetime module scipt

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,8 +2,8 @@ from datetime import datetime
 import datetime
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
-    print(f'Current date and time: {current_date.strftime("%Y-%m-%d %H:-%M:-%S")}')
+    current_date = datetime.datetime.now()                
+    print(f'Current date and time: {current_date.strftime("%Y-%m-%d %H:%M:%S")}')
 
 def calculate_future_date(number_of_days):
     current_date = datetime.datetime.now()
