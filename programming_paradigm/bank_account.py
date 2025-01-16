4d3990f4aa6691c46957883737d02954b00a009c
IbraamSaad <ebramb37@gmail.com>
2025-01-17 00:33:42 +0200

    Bank account script

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAcount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+    
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if self.account_balance < amount:
+           self.account_balance -= amount
+           return True
+        else:
+           raise "Insufficient funds."
+           
+
+    def display_balance(self):
+        return f" Current Balance: ${self.account_balance}"
+    
