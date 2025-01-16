9df1f8d3c28d948250eb03904faa310af711129f
IbraamSaad <ebramb37@gmail.com>
2025-01-17 00:39:12 +0200

    Bank account script

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,8 @@
 class BankAccount:
-    def __init__(self, account_balance):
+    def __init__(self, account_balance, intialized_balance = 0):
         self.account_balance = account_balance
+        self.intialized_balance = intialized_balance
+        self.account_balance += self.intialized_balance
     
     def deposit(self, amount):
         self.account_balance += amount
