56f88ebe97c64105986743b604c0d35ea09e09ea
IbraamSaad <ebramb37@gmail.com>
2025-01-18 21:36:58 +0200

    Bank account script

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,9 +7,10 @@ class BankAccount:
         self.account_balance += amount
 
     def withdraw(self, amount):
-        if self.account_balance > amount:
-            self.account_balance -= amount
-            print(f"Withdrew: ${amount}")
+        if self.account_balance >= amount:
+            return True 
+        else:
+            return False    
 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
