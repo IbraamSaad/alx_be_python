0648ef9e0ba9b79fc0f571d44db039b90ced5d59
IbraamSaad <ebramb37@gmail.com>
2025-01-26 00:40:04 +0200

    library system script

---
+++ oop/library_system.py
@@ -0,0 +1,41 @@
+class Book:
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}"
+
+
+class EBook(Book):
+    def __init__(self, title, author, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"{super().__str__()}, File Size: {self.file_size} MB"
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"{super().__str__()}, Page Count: {self.page_count}"
+
+class Library:
+    def __init__(self):
+        self.books = []
+    
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print (book)
+   
+   
+   
